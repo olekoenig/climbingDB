f0be231a897f677938cc1745dbb325a912c2f363
Ole Koenig <ole.koenig@fau.de>
2019-08-11 14:50:00 +0200

    ADD: if no arguments in main: call getAllRoutes (ordered by grade)

--- main.py
+++ main.py
@@ -27,42 +27,47 @@ def arguments():
 
 
 def main():
-     # Get command line arguments
-     args = arguments()
+    # Get command line arguments
+    args = arguments()
+    
+    # Import the CSV file
+    db=ClimbingQuery()
+    if args.stars==None and args.area==None and args.grade==None:
+        # I'm sure this if statement can be made nicer...
+        print(db.getAllRoutes()) # ordered by grade!
+    else:
+        routes = db.getFilteredRoutes(area = args.area,
+                                      stars= args.stars,
+                                      grade= args.grade)
+        print(routes)
+        
+    # db.get_crag_info("Wüstenstein")
+    # db.give_os_F("9-","Frankenjura")
+    # db.sort_by_date()
+        
 
-     # Import the CSV file
-     db=ClimbingQuery()
-     routes = db.getFilteredRoutes(area = args.area,
-                                   stars= args.stars,
-                                   grade= args.grade)
-     print(routes)
-
-     # db.get_crag_info("Wüstenstein")
-     # db.give_os_F("9-","Frankenjura")
-     # db.sort_by_date()
-
-
-     #To be plotted:
-     # Onsights, Flashs in different color
-     # 3D plot: x=time, y=grade, z=number
-     # from grade import French # Plots are in French grading
-     # x = list(French.keys())
-     # x.sort()
-     # y = [len(list(filter(lambda x: x.grade.value==grade, routelist))) for grade in x]
-     # data = [go.Bar(x=x,y=y)]
-     # #plotly.offline.plot(data, auto_open=True)
-     # #plt.bar(range(len(y)), y, align='center')
-     # #plt.xticks(range(len(y)), x, size='small')
-     # # setup the figure and axes
-     # fig = plt.figure(figsize=(8, 3))
-     # ax1 = fig.add_subplot(121, projection='3d')
-     # bottom = 0
-     # width = depth = 1
-     # top = max(y)
-     # ax1.bar3d(x, y, bottom, width, depth, top, shade=True)
-     # ax1.set_title('Shaded')
-     # plt.show()
-
-
+    #To be plotted:
+    # Onsights, Flashs in different color
+    # 3D plot: x=time, y=grade, z=number
+    # from grade import French # Plots are in French grading
+    # x = list(French.keys())
+    # x.sort()
+    # y = [len(list(filter(lambda x: x.grade.value==grade, routelist))) for grade in x]
+    # data = [go.Bar(x=x,y=y)]
+    # #plotly.offline.plot(data, auto_open=True)
+    # #plt.bar(range(len(y)), y, align='center')
+    # #plt.xticks(range(len(y)), x, size='small')
+    # # setup the figure and axes
+    # fig = plt.figure(figsize=(8, 3))
+    # ax1 = fig.add_subplot(121, projection='3d')
+    # bottom = 0
+    # width = depth = 1
+    # top = max(y)
+    # ax1.bar3d(x, y, bottom, width, depth, top, shade=True)
+    # ax1.set_title('Shaded')
+    # plt.show()
+    
+    
 if __name__ == "__main__":
-     main()
+    main()
+        
