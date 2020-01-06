5482eac61e10af4ff24912c23a9bb5d0d7e592d7
Ole Koenig <ole.koenig@fau.de>
2020-01-06 12:17:27 +0100

    Update histogram plotting printAllRoutes()

--- code/climbingQuery.py
+++ code/climbingQuery.py
@@ -1,4 +1,4 @@
-from grade import Grade
+from grade import Grade, French
 # from route import Route
 import pandas
 import matplotlib.pyplot as plt
@@ -18,11 +18,11 @@ class ClimbingQuery:
 
 
      def _import_routes(self):
-          """
-          Import the CSV file.
-          :returns: data (Pandas data frame)
-          
+          """Import the CSV file.
+
           .. note:: To be changed to SQL database.
+
+          :returns: data (Pandas data frame)
           """
           # Import CSV file (should be changed to SQL query)
           data = pandas.read_csv("../data/routes.csv",
@@ -38,8 +38,8 @@ class ClimbingQuery:
           
      
      def getFlashes(self, area=None, grade=None):
-          """
-          A function to return a route list of flashed routes in an area.
+          """A function to return a route list of flashed routes in an area.
+
           :param grade: The grade, e.g. '7c' or '9' or '5.12d'
           :param area: Area name, e.g. "Frankenjura"
           """
@@ -48,8 +48,8 @@ class ClimbingQuery:
 
      
      def getOnsights(self, area=None, grade=None):
-          """
-          A function to return a route list of onsighted routes in an area.
+          """A function to return a route list of onsighted routes in an area.
+
           :param grade: The grade, e.g. '7c' or '9' or '5.12d'
           :param area: Area name, e.g. "Frankenjura"
           """
@@ -58,18 +58,24 @@ class ClimbingQuery:
 
 
      def printRouteNumbers(self):
-          """
-          Prints the number of routes in each grade and plots a histogram.
-          """
+          """Prints the number of routes in each grade and plots a histogram."""
           # Copy the data frame, otherwise it is overridden
           routes = self.data[self.data.project!="X"].copy()
           print("Number of Routes >= 8a: \t{}".format(len(routes[routes.ole_grade>=Grade("8a").conv_grade()])))
           print("Total number of routes: \t{}".format(len(routes)))
 
-          # Plot the route distribution as matplotlib object (internal pandas function)
-          # routes.hist(column="ole_grade",bins=30)
-          # plt.show()
+          # Plot the route distribution with internal pandas function
+          # routes.hist(column="ole_grade",bins=30) ; plt.show()
+          # Plot all grades (also slash grades)
+          x = list(French.keys()) #.sort()
+          y = [len(routes[routes.ole_grade==Grade(grade).conv_grade()]) for grade in x]
+          plt.bar(x,y) ; plt.show()
 
+          # Plot route distribution
+          # Need to account for the slash grades, so one cannot only
+          # plot the grades of, e.g., 8a, 8a+ etc. but has to get also
+          # the routes grated 8a/8a+.
+          # A route gradet 8a/8a+ belongs to the 8a bar.
           fig, ax = plt.subplots(figsize=(20,10))
           grades=('4a','5a','6a','6b','6c','7a','7a+','7b','7b+',
                   '7c','7c+','8a','8a+','8b','8b+','8c')
@@ -77,14 +83,18 @@ class ClimbingQuery:
           for ii in range(1,len(grades)-1,1):
                g=Grade(grades[ii]).conv_grade()
                gup=Grade(grades[ii+1]).conv_grade()
-               glo=Grade(grades[ii-1]).conv_grade()
-               ax.bar(g, len(routes[routes.ole_grade==g]))
-
+               # Get all routes >= 8a
+               len_glo=len(routes[routes.ole_grade>=g])
+               # Get all routes >=8a+
+               len_gup=len(routes[routes.ole_grade>=gup])
+               # Number of routes is set of len_glo AND len_gup
+               len_tot=len_glo-len_gup
+               ax.bar(g, len_tot, color="blue")
           ax.set_xticks(pos_x)
           ax.set_xticklabels(grades)
           plt.show()
           return None
-
+     
           
      def getAllRoutes(self, area=None):
           """Returns the complete route list in an area.  Has to be a pandas
@@ -93,11 +103,16 @@ class ClimbingQuery:
           """
           return self.getFilteredRoutes(area=area).sort_values(by=["ole_grade"])#.__str__()
 
+
+     def getAllAreas(self):
+          """To be implemented, should be exported to location.py at
+          some point. Needed for a drop-down menu to select routes of
+          a certain area."""
+          return False
+     
      
      def getProjects(self, area=None):
-          """
-          Returns the project list in an area.
-          """
+          """Returns the project list in an area."""
           projects = self.data[self.data.project=="X"]
           if area:
                projects = projects[projects.area==area]
@@ -108,12 +123,12 @@ class ClimbingQuery:
                            stars=None, operation="=="):
           """
           Return a route list under the applied filters.
+
           :param area: Area name, e.g. 'Frankenjura'
           :param grade: Grade, e.g. '8a' or '9+/10-'
           :param style: Onsight 'o.s.' or Flash 'F'
           :param stars: Number of stars [0,1,2,3]
-          :param operation: logic operation applied to grade (>,<,==)
-                           [default: ==], currently supported: ==,>=
+          :param operation: logic operation applied to grade [default: ==], currently supported: ==,>=
           :returns: pandas data frame
           """
           kwargs = {'area': area,
@@ -129,6 +144,7 @@ class ClimbingQuery:
           for k,v in kwargs.items():
                if v and k=="stars" or (k=="ole_grade" and operation==">="):
                     # applies if stars set: display routes with stars>=value
+                    # or applies if operation is larger-equal and grade set
                     routes = routes[routes[k] >= v]
                elif v:
                     routes = routes[routes[k] == v]
@@ -137,8 +153,7 @@ class ClimbingQuery:
      
      
      def getCragInfo(self, cragname):
-          """
-          Prints the info about a crag.
+          """Prints the info about a crag.
 
           .. note:: To be changed to SQL query.
           """
@@ -150,9 +165,7 @@ class ClimbingQuery:
           
           
      def getRouteInfo(self,routename):
-          """
-          Get the logged information about a route.
-          """
+          """Get the logged information about a route."""
           info=self.data[self.data.name==routename]
           if info.notes.any()==False:
                exit("No information about the route "+routename+" available")
